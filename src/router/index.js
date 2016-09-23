7bcc22ac79315b25528cda2edb9922c68652f74c
IMustTo <cxjbinfo@gmail.com>
2016-09-23 16:16:07 +0800

    :pencil: use debug

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,18 @@
+import debug from 'debug';
 import Router from 'koa-router';
 import path from 'path';
 
 const api = new Router();
+const log = debug('router');
 
 api
-  .get('/', async (ctx) => {
+  .get('/test', async (ctx) => {
     // 渲染模板
-    console.log(1);
+    log(ctx.toJSON());
     ctx.body = 'test!';
   })
   .get('/index', async (ctx) => {
-    console.log(path.resolve(__dirname, '../../index.html'));
+    log(path.resolve(__dirname, '../../index.html'));
     // 发送静态文件
     await ctx.send(ctx, 'index.html', { root: path.resolve(__dirname, '../../') });
   });
